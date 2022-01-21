0da5538c6b28a569b51f837004c634f0949bbffb
kello22e <36242568+kello22e@users.noreply.github.com>
2022-01-21 15:04:46 -0500

    file edits

--- R/FC2C_medial_test.R
+++ R/FC2C_medial_test.R
@@ -1,31 +1,21 @@
-library(nat)
-library(natverse)
-library(neuprintr)
-library(nat.templatebrains)
-library(nat.jrcbrains)
-register_saalfeldlab_registrations()
-
-
-#establish neuprint database
-conn = neuprint_login(server= "https://neuprint.janelia.org/",
-                      token= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImtlbGxvMjJlQG10aG9seW9rZS5lZHUiLCJsZXZlbCI6Im5vYXV0aCIsImltYWdlLXVybCI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FBVFhBSnoxcWZuYXZJTHRHd1FyWGY2OEZGYVdTcFdhU09IMTIwTlRsSFU9czk2LWM_c3o9NTA_c3o9NTAiLCJleHAiOjE4MTA1MTA0MDd9.7IWz4boFJ2iDUmr0zNIjFXGNhSa1KWESateQovBExEE")
-options(neuprint_dataset="hemibrain:v1.2.1")
+source("R/startup/packages.R")
 
 #get all FC1 neurons
-FC1.info <- neuprint_search("type:FC2C.*")
+FC2C.info <- neuprint_search("type:FC2C.*")
 #FC1_split <- split(FC1.info, FC1.info$type)
 
 #get the neuron skeletons
-FC2C_skel = neuprint_read_neurons(FC1.info$bodyid)
+FC2C_skel = neuprint_read_neurons(FC2C.info$bodyid)
 
 
 #FC1A
 FC2C.jrc2018f = xform_brain(FC2C_skel*8/1000, reference="JRC2018F", sample="JRCFIB2018F")
 FC2C.jrc2018u = xform_brain(FC2C.jrc2018f, reference='JRC2018U', sample='JRC2018F')
 
-#nopen3d()
+nopen3d()
 #plot FC1 neurons with different colors
-#plot3d(FC2C.jrc2018u,lwd=3,col='black',WithNodes=FALSE,soma=FALSE)
+plot3d(FC2C.jrc2018u,lwd=3,col='black',WithNodes=FALSE,soma=FALSE)
+plot3d(JRC2018U)
 
 #save the FC2C to a .nrrd file 
-write.nrrd(FC2C.jrc2018u,'Desktop/FC2C_JRC2018')
+write.nrrd(as.im3d(FC2C.jrc2018u),'data/FC2C_JRC2018.nrrd')
---
+++ R/startup/packages.R
@@ -0,0 +1,11 @@
+library(nat)
+library(natverse)
+library(neuprintr)
+library(nat.templatebrains)
+library(nat.jrcbrains)
+register_saalfeldlab_registrations()
+
+#establish neuprint database
+conn = neuprint_login(server= "https://neuprint.janelia.org/",
+                      token= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImtlbGxvMjJlQG10aG9seW9rZS5lZHUiLCJsZXZlbCI6Im5vYXV0aCIsImltYWdlLXVybCI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FBVFhBSnoxcWZuYXZJTHRHd1FyWGY2OEZGYVdTcFdhU09IMTIwTlRsSFU9czk2LWM_c3o9NTA_c3o9NTAiLCJleHAiOjE4MTA1MTA0MDd9.7IWz4boFJ2iDUmr0zNIjFXGNhSa1KWESateQovBExEE")
+options(neuprint_dataset="hemibrain:v1.2.1")
