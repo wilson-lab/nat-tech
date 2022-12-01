47c1e46bb28b61d9f93109f30d4f1df934f3b6db
kello22e <36242568+kello22e@users.noreply.github.com>
2022-12-01 16:51:19 -0500

    fix source issues

--- R/parameters.R
+++ R/parameters.R
@@ -1,5 +1,4 @@
 # USER enter the path to your unregistered data here:
-
 registration_folder = "/Users/wilsonlab/Desktop/Registration"
 data_folder = "/Volumes/Neurobio/Wilson\ Lab/Emily"
 raw_data = file.path(data_folder,"unprocessed")
