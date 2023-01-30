c970aa387db12014c149872a4419a166e4008bde
kello22e <36242568+kello22e@users.noreply.github.com>
2023-01-30 11:23:36 -0500

    added macros to parameters

--- R/parameters.R
+++ R/parameters.R
@@ -3,3 +3,6 @@ registration_folder = "/Users/wilsonlab/Desktop/Registration"
 data_folder = "/Volumes/Neurobio/Wilson\ Lab/Emily"
 raw_data = file.path(data_folder,"unprocessed")
 processed_data = file.path(data_folder,"processed")
+macro1 = "/Users/wilsonlab/Documents/GitHub/nat-tech/R/macros/create_registration_images.ijm"
+macro2 = "/Users/wilsonlab/Documents/GitHub/nat-tech/R/macros/create_composite.ijm"
+
