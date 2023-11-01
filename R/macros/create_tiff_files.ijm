9dd9c778a4bd89e808b738a4ad01cad49ad3af09
srenauld1 <sophia_renauld@hms.harvard.edu>
2023-11-01 14:09:55 -0400

    Added SMR specific AN matching code

--- R/macros/create_tiff_files.ijm
+++ R/macros/create_tiff_files.ijm
@@ -1,63 +1,47 @@
-arg = getArgument();
-//new argument combines the registration folder location and the file location. you need to split at the "-" to get both paths
-// for testing: arg = "/Users/wilsonlab/Desktop/Registration/Images-/Volumes/Neurobio/wilsonlab/Emily/unprocessed/20211203_JRC2018U_LT51_VT049899AD_VT034811GDBD_1.tif"
-arg_split = split(arg,"-");
-
-//assign registration folder location to reg_folder and .tif location to path
-reg_folder = arg_split[0];
-path = arg_split[1];
+path = getArgument();
 name = File.getName(path);
 
 if(name=="") exit ("No argument");
 
+//path = "/Users/sophiarenauld/Documents/janelia_acending_neuron_stacks/brain/SS52147-20181026_47_C4-m-20x-brain-Split_GAL4-JRC2018_Unisex_20x_HR-aligned_stack.h5j";
+//name = File.getName(arg);
+
+//get folder path
+parent_path = File.getParent(path); 
+
+//gets the file list in the parent folder
+file_list = getFileList(parent_path);
+
+//open file
 open(path);
 selectWindow(name);
+
+
+//split channels
 run("Split Channels");
-//add functionality for Gal4
-//this will mean that naming correctly is essential 20220322(0)_JRCU2018(1)_FC1(2)_VT033647AD(3)_R64A11GDBD(4)_1(5).tif
-//20220322(0)_JRCU2018(1)_FC1(2)_R64A11Gal4(3)_1(4).tif
+
+//select red channels
+selectWindow("C1-" + name);
+
+//make naming easier
 x = split(name,".");
 no_tif = x[0];
 name_array = split(no_tif,"_");
 
-if(name_array.length == 6){
-	celltype = name_array[2];
-	genotype = name_array[3] + "_" + name_array[4] + "_";
-	folder_path = "/" + genotype + name_array[5] + "/";
-	File.makeDirectory(reg_folder + "/" + genotype + name_array[5]);
-	
-}else if(name_array.lenth == 5){
-	celltype = name_array[2];
-	genotype = name_array[3];
-	folder_path = "/" + genotype + name_array[4] + "/";
-	File.makeDirectory(reg_folder + "/" + genotype + name_array[5]);
-}
+// save tiff of red channel only
+//new_name = no_tif + ".tif" ;
+//saveAs("Tiff", parent_path+ "/" + new_name);
 
-//handle 3 channels
-list = getList("image.titles");
+new_name = no_tif;
+//run("Nrrd ...", "nrrd="+new_name);
+run("32-bit");
+run("Nrrd ... ", "nrrd=[" + parent_path + "/" + new_name + ".nrrd" + "]");
 
-if(list.length==2){
-	selectWindow("C2-" + name);
-	//for o2 folder should be "/Volumes/Neurobio/Wilson Lab/Emily/unprocessed/Registration/Images"
-	run("Nrrd ... ", "nrrd=" + reg_folder + "/" + folder_path + celltype + "_" + genotype + "01.nrrd");
 
-	selectWindow("C1-" + name);
-	//for o2 folder should be "/Volumes/Neurobio/Wilson Lab/Emily/unprocessed/Registration/Images"
-	run("Nrrd ... ", "nrrd=" + reg_folder + "/" + folder_path + celltype + "_" + genotype + "02.nrrd");
-	
-}else if(list.length == 3){
-	selectWindow("C1-" + name);
-	//for o2 folder should be "/Volumes/Neurobio/Wilson Lab/Emily/unprocessed/Registration/Images"
-	run("Nrrd ... ", "nrrd=" + reg_folder + "/" + folder_path + celltype + "_" + genotype + "01.nrrd");
+//select original window and close
+//selectWindow(new_name);
+close();
 
-	selectWindow("C2-" + name);
-	//for o2 folder should be "/Volumes/Neurobio/Wilson Lab/Emily/unprocessed/Registration/Images"
-	run("Nrrd ... ", "nrrd=" + reg_folder + "/" + folder_path + celltype + "_" + genotype + "02.nrrd");
-	
-	selectWindow("C3-" + name);
-	//for o2 folder should be "/Volumes/Neurobio/Wilson Lab/Emily/unprocessed/Registration/Images"
-	run("Nrrd ... ", "nrrd=" + reg_folder + "/" + folder_path + celltype + "_" + genotype + "03.nrrd");
-}
-
-run("Close All");
-exit();
+run("Quit");
+//My macro is not ending
+//exit();
--- R/projects/an_line_matching.R
+++ R/projects/an_line_matching.R
@@ -4,7 +4,7 @@ source("R/startup/functions.R")
 
 #paths we will need
 imagepath = "/Users/sophiarenauld/Documents/janelia_acending_neuron_stacks/brain"
-lineimages = "/Users/sophiarenauld/Documents/janelia_acending_neuron_stacks/brain/neuron_channel"
+lineimages = "/Users/sophiarenauld/Documents/janelia_acending_neuron_stacks/rgl_plots"
 flywirepath ="/Users/sophiarenauld/Documents/janelia_acending_neuron_stacks/brain_flywire"
 processed_ans = "/Users/sophiarenauld/Documents/janelia_acending_neuron_stacks/processed_ans"
 matching_folder = "/Users/sophiarenauld/Documents/janelia_acending_neuron_stacks/matched_ans"
@@ -18,12 +18,12 @@ ft.an <- subset(ft, ft$cell_class=="AN")
 #read in AN as mesh file and turn into vector cloud (can do lots of things with meshes)
 #REMEMBER that the light level data is not actually in JRC2018U  
 ans.dps = nat::neuronlist()
-for (id in ft.an$root_id[1:5]) {
+for (id in ft.an$root_id) {
   mesh = fafbseg::read_cloudvolume_meshes(id)
   #transform neuron into the correct template space
   flywire.reg = nat.templatebrains::xform_brain(mesh, reference="JRC2018U", sample="FAFB14")
   flywire.reg[[1]] = Rvcg::vcgQEdecim(flywire.reg[[1]], percent = 0.001)
-  dps = dotprops(flywire.reg)
+  dps = nat::dotprops(flywire.reg)
   ans.dps = nat::union(ans.dps, dps)
 }
 # save all em ans as dots
@@ -33,37 +33,66 @@ save(ans.dps, file = savefile)
 #get image data
 lm.images = list.files(imagepath, full = TRUE, pattern = "\\.h5j")
 #regex - match strings, have to remove contexts of characters (\\)
-contents <- list.files(lineimages, full.names = TRUE)
-runMacro(macro = "/Users/sophiarenauld/Documents/GitHub/nat-tech/R/macros/create_max_projection_serial.ijm", 
-         macroArg = contents[2], 
-         headless = FALSE,
-         batch = FALSE,
-         MinMem = "100m",
-         MaxMem = "25000m",
-         IncrementalGC = TRUE,
-         Threads = NULL,
-         fijiArgs = NULL,
-         javaArgs = NULL, 
-         ijArgs = NULL,
-         fijiPath = neuronbridger:::fiji(),
-         DryRun = FALSE)
-tiff.images = list.files(lineimages, full = TRUE, pattern = "\\.h5j")
-for (file in tiff.images[1:10]) {
-  pc = raster::raster(file)
-  dps = dotprops(pc)
-  lm.ans.dps = nat::union(lm.ans.dps, dps)
+#no longer need contents <- list.files(lineimages, full.names = TRUE)
+for (pic in lm.images) {
+  runMacro(macro = "/Users/sophiarenauld/Documents/GitHub/nat-tech/R/macros/create_tiff_files.ijm", 
+           macroArg = pic,
+           headless = FALSE,
+           batch = FALSE,
+           MinMem = "100m",
+           MaxMem = "25000m",
+           IncrementalGC = TRUE,
+           Threads = NULL,
+           fijiArgs = NULL,
+           javaArgs = NULL, 
+           ijArgs = NULL,
+           fijiPath = neuronbridger:::fiji(),
+           DryRun = FALSE)
 }
+
+lm.ans.dps = nat::neuronlist()
+#need to make this  macro stop after it is done
+nrrd.images = unique(list.files(imagepath, full = TRUE, pattern = "\\.nrrd"))
+for (file in nrrd.images) {
+  message("working on ", file)
+  pc = nat::read.im3d(file)
+  xlookup = attr(pc,"x")
+  ylookup = attr(pc,"y")
+  zlookup = attr(pc,"z")
+  m = reshape2::melt(pc, varnames = c("Row", "Column", "Dimension"), value.name = "brightness")
+  points = m %>%
+    dplyr::filter(brightness > stats::quantile(pc, 0.9999)) %>%
+    dplyr::rename(x=Row, y=Column, z=Dimension) %>%
+    dplyr::mutate(x=xlookup[x], y=ylookup[y], z=zlookup[z]) %>%
+    dplyr::select(-brightness)
+  ## Sanity check
+  #I <- nat::as.im3d(nat::xyzmatrix(points), voxdims = c(0.519,0.52,1), BoundingBox = nat::boundingbox(nat.flybrains::JRC2018U))
+  #nat::write.nrrd(I, "~/Desktop/test.nrrd")
+  dps = nat::dotprops(points)
+  entry = nat::neuronlist(dps, DATAFRAME = data.frame(file=basename(file)))
+  names(entry) = basename(file)
+  lm.ans.dps = nat::union(lm.ans.dps, entry)
+  open3d(zoom=0.4581118, userMatrix = structure(c(0.99932849407196, -0.00221055885776877, -0.0365745350718498, 
+                                                  0, -0.0118458131328225, -0.964066743850708, -0.265395909547806, 
+                                                  0, -0.0346736125648022, 0.2656509578228, -0.963445603847504, 
+                                                  0, 0, 0, 0, 1), .Dim = c(4L, 4L)), windowRect = c(0L, 45L, 1425L, 812L))
+  points3d(points)
+  plot3d(JRC2018U, alpha =0.1, color="gray")
+  rgl.snapshot(file=file.path(lineimages, gsub("\\.nrrd", ".png", basename(file))))
+  close3d()
+}
+
 #save all light level ans as dots
 savefile = file.path(processed_ans, "lm_ans_dps.rda")
-save(lm_ans.dps, file = savefile)
+save(lm.ans.dps, file = savefile)
 
 #step 3 - run Nblast
 #uncomment in new page
-#load(file.path(processed_ans, "lm_ans_dps.rda"))
-#load(file.path(processed_ans, "ans_dps.rda"))
+load(file.path(processed_ans, "lm_ans_dps.rda"))
+load(file.path(processed_ans, "ans_dps.rda"))
 
 #run nblast
-nb = nat.nblast::nblast(ans.dps, ans.dps, normalised = TRUE)
+nb = nat.nblast::nblast(lm.ans.dps, ans.dps, normalised = TRUE)
 colnames(nb) <- paste0("id",as.character(colnames(nb)))
 rownames(nb) <- paste0("id",as.character(rownames(nb)))
 nb.df <- reshape2::melt(nb, varnames = c("Row", "Column"), value.name = "nblast_score")
@@ -79,7 +108,7 @@ nb.hits.df <- nb.df %>%
 readr::write_csv(nb.hits.df, file = file.path(processed_ans, "ans_nblast_results.csv"))
 
 #save just the an em images with a hit
-top.ids = subset(nb.hits.df, nblast_score > 0.1)$root_id
+top.ids = subset(nb.hits.df, nblast_score > 0.1)$flywire
 for (an.id in top.ids){
   flywireid_to_nrrd(flywire_id = an.id, 
                     cell_type = an.id, 
@@ -118,3 +147,11 @@ for (ln in nb.hits.df$line_name){
 #plot3d(elmr::FAFB.surf, alpha = 0.1, col = "gray")
 #plot3d(elmr::JRC2018U) for switching brain space
 #> plot3d(mesh)
+#> 
+#> 
+# resave neurons as tiffs
+
+name = "/Users/sophiarenauld/Documents/janelia_acending_neuron_stacks/brain/SS46233-20180202_43_E5-m-20x-brain-Split_GAL4-JRC2018_Unisex_20x_HR-aligned_stack.h5j"
+runMacro(macro = "/Users/sophiarenauld/Documents/GitHub/nat-tech/R/macros/create_tiff_files.ijm", 
+         macroArg = name, 
+         )
